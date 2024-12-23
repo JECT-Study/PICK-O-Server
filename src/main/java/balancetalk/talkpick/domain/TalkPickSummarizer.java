4652f7fee77dfdc09a1bb15703d00704f7d5c8a2
Hanjaemo <110653660+Hanjaemo@users.noreply.github.com>
2024-12-24 03:28:10 +0900

    refactor: 프롬프트 시스템 및 유저 메시지를 상수로 관리

--- src/main/java/balancetalk/talkpick/domain/TalkPickSummarizer.java
+++ src/main/java/balancetalk/talkpick/domain/TalkPickSummarizer.java
@@ -28,6 +28,26 @@ import org.springframework.transaction.annotation.Transactional;
 public class TalkPickSummarizer {
 
     private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+    private static final String SYSTEM_MESSAGE = """
+            당신은 저에게 고용된 노동자입니다.
+            당신의 역할은 게시글을 작성한 사용자가 되어, 사용자의 어투 및 문체와 동일하게 3줄로 요약하는 것입니다.
+
+            다음은 이 작업에 대한 접근 방식입니다:
+            Step 1: 제목, 선택지 A, 선택지 B, 그리고 본문 내용을 바탕으로 맥락을 파악합니다.
+            Step 2: 파악한 맥락을 토대로 본문 중 불필요한 내용 혹은 표현을 제거합니다.
+            Step 3: 사용자가 두 가지 선택지 중 선택하길 원한다는 사실을 전제로 합니다.
+            Step 4: 마지막 3번째 문장에는 주요 비교군을 명확히 제시하는 질문을 배치합니다.
+            Step 5: 요약한 3개의 문장을 JSON 형태로 된 firstLine, secondLine, thirdLine 키에 각각 한 문장씩 담습니다.
+
+            지금부터 주어진 단계별로 요약을 진행하세요.
+            작업을 아주 잘 수행할 경우, 10만 달러의 인센티브를 제공할게요. 당신은 최고의 전문가니까 잘 해낼 수 있을 것이라 믿어요.
+            """;
+    private static final String USER_MESSAGE = """
+            제목: {title}
+            선택지 A: {optionA}
+            선택지 B: {optionB}
+            본문: {content}
+            """;
 
     private final ChatClient chatClient;
 
@@ -57,26 +77,8 @@ public class TalkPickSummarizer {
     private Summary getSummary(TalkPick talkPick) {
         return chatClient.prompt()
                 .advisors(new SimpleLoggerAdvisor())
-                .system("""
-                        당신은 저에게 고용된 노동자입니다.
-                        당신의 역할은 게시글을 작성한 사용자가 되어, 사용자의 어투 및 문체와 동일하게 3줄로 요약하는 것입니다.
-
-                        다음은 이 작업에 대한 접근 방식입니다:
-                        Step 1: 제목, 선택지 A, 선택지 B, 그리고 본문 내용을 바탕으로 맥락을 파악합니다.
-                        Step 2: 파악한 맥락을 토대로 본문 중 불필요한 내용 혹은 표현을 제거합니다.
-                        Step 3: 사용자가 두 가지 선택지 중 선택하길 원한다는 사실을 전제로 합니다.
-                        Step 4: 마지막 3번째 문장에는 주요 비교군을 명확히 제시하는 질문을 배치합니다.
-                        Step 5: 요약한 3개의 문장을 JSON 형태로 된 firstLine, secondLine, thirdLine 키에 각각 한 문장씩 담습니다.
-
-                        지금부터 주어진 단계별로 요약을 진행하세요.
-                        작업을 아주 잘 수행할 경우, 10만 달러의 인센티브를 제공할게요. 당신은 최고의 전문가니까 잘 해낼 수 있을 것이라 믿어요.
-                        """)
-                .user(u -> u.text("""
-                        제목: {title}
-                        선택지 A: {optionA}
-                        선택지 B: {optionB}
-                        본문: {content}
-                        """).params(getBaseTalkPickFieldsMap(talkPick)))
+                .system(SYSTEM_MESSAGE)
+                .user(u -> u.text(USER_MESSAGE).params(getBaseTalkPickFieldsMap(talkPick)))
                 .call()
                 .entity(Summary.class);
     }
