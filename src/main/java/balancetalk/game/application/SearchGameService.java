e22442b6c307bbf13ed7bad4bd444cee36e708eb
king_0417 <73704053+gywns0417@users.noreply.github.com>
2025-01-15 21:07:09 +0900

    feat: 밸런스게임 검색 로직 개선

--- src/main/java/balancetalk/game/application/SearchGameService.java
+++ src/main/java/balancetalk/game/application/SearchGameService.java
@@ -32,9 +32,18 @@ public class SearchGameService {
     private static final int MINIMUM_SEARCH_LENGTH = 2;
 
     public Page<SearchGameResponse> search(String query, Pageable pageable, String sort) {
-        List<Game> resultList = new CopyOnWriteArrayList<>();
 
-        // 검색어가 공백이거나 2자 미만일 경우 예외 처리
+        validateQuery(query);
+        String queryWithoutSpaces = removeSpaces(query);
+
+        Page<Game> pageResult = gameRepository.searchAll(query, queryWithoutSpaces, sort, pageable);
+
+        List<SearchGameResponse> responses = convertToResponse(pageResult.getContent());
+
+        return new PageImpl<>(responses, pageable, pageResult.getTotalElements());
+    }
+
+    private void validateQuery(String query) {
         if (query.isBlank()) {
             throw new BalanceTalkException(BALANCE_GAME_SEARCH_BLANK);
         }
@@ -42,37 +51,6 @@ public class SearchGameService {
         if (query.replace(" ", "").length() < MINIMUM_SEARCH_LENGTH) {
             throw new BalanceTalkException(BALANCE_GAME_SEARCH_LENGTH);
         }
-
-        // 1. 완전 일치 검색
-        searchExactMatch(query, resultList, sort);
-
-        // 2. 공백 제거한 단어로 완전 일치 검색
-        String queryWithoutSpaces = removeSpaces(query);
-        searchExactMatch(queryWithoutSpaces, resultList, sort);
-
-        // 3. 자연어 검색
-        searchNaturalLanguage(query, resultList, sort);
-
-        List<SearchGameResponse> responses = convertToResponse(resultList);
-
-        // 페이지네이션 적용(페이지 번호, 페이지 크기, 총 게임수는 컨트롤러에서 제공
-        int start = (int) pageable.getOffset();
-        int end = Math.min((start + pageable.getPageSize()), responses.size());
-        return new PageImpl<>(responses.subList(start, end), pageable, responses.size());
-    }
-
-    private void searchExactMatch(String query, List<Game> resultList, String sort) {
-        List<Game> results = gameRepository.searchExactMatch(query);
-
-        sort(results, sort);
-        resultList.addAll(results);
-    }
-
-    private void searchNaturalLanguage(String query, List<Game> resultList, String sort) {
-        List<Game> results = gameRepository.searchNaturalLanguage(query);
-
-        sort(results, sort);
-        resultList.addAll(results);
     }
 
     private void sort(List<Game> resultList, String sort) {
